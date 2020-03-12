04120400f4a7300545b6330f8c71c0cb4985586f
Deeprand <qubicand@gmail.com>
2020-03-12 15:50:33 +0800

    add: mind map of `1.6_晶体的X射线衍射`

---
+++ mindmap/1-6_晶体的X射线衍射.mm
@@ -0,0 +1,183 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="1.6 &#x6676;&#x4f53;&#x7684; X &#x5c04;&#x7ebf;&#x884d;&#x5c04;" LOCALIZED_STYLE_REF="default" FOLDED="false" ID="ID_971870573" CREATED="1583309342490" MODIFIED="1583551489079"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false" show_note_icons="false" show_notes_in_map="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" BORDER_WIDTH="1.0 px">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="24" RULE="ON_BRANCH_CREATION"/>
+<font NAME="Kai" SIZE="21" BOLD="false"/>
+<node TEXT="1 &#x5f15;&#x8a00;" LOCALIZED_STYLE_REF="defaultstyle.details" POSITION="right" ID="ID_644501243" CREATED="1583313406454" MODIFIED="1583418223123">
+<edge COLOR="#7c7c00"/>
+<font BOLD="true"/>
+<node TEXT="\latex \jlmText{&#x9009;&#x7528; X &#x5c04;&#x7ebf;&#x7684;&#x539f;&#x56e0;: &#x6ce2;&#x957f;&#x4e3a;} $10^{-8}\ \mathrm{cm}$" ID="ID_1198084771" CREATED="1583309510242" MODIFIED="1583416021065" FORMAT="STANDARD_FORMAT">
+<font NAME="SansSerif" SIZE="10" BOLD="false"/>
+<attribute_layout NAME_WIDTH="19.49999941885473 pt" VALUE_WIDTH="19.49999941885473 pt"/>
+</node>
+<node TEXT="&#x9009;&#x7528; X &#x5c04;&#x7ebf;&#x7684;&#x539f;&#x56e0;&#x60f3;&#x6cd5;&#x7684;&#x63d0;&#x51fa;: &#x52b3;&#x5384;, 1917" ID="ID_1360486255" CREATED="1583319115390" MODIFIED="1583458740322" FORMAT="STANDARD_FORMAT">
+<font BOLD="false"/>
+</node>
+<node TEXT="&#x5e03;&#x62c9;&#x683c;&#x7236;&#x5b50;: &#x5e03;&#x62c9;&#x683c;&#x5b9a;&#x5f8b;, 1912" ID="ID_311596904" CREATED="1583313836882" MODIFIED="1583460725395"/>
+</node>
+<node TEXT="2 &#x6676;&#x4f53;&#x884d;&#x5c04;" POSITION="right" ID="ID_396766957" CREATED="1583314013258" MODIFIED="1583460813727">
+<edge COLOR="#ff0000"/>
+<font BOLD="true"/>
+<node TEXT="1). &#x884d;&#x5c04;&#x7684;&#x57fa;&#x672c;&#x65b9;&#x6cd5;" ID="ID_1153142523" CREATED="1583314089546" MODIFIED="1583457806037">
+<node TEXT="(1) X &#x5c04;&#x7ebf;&#x884d;&#x5c04;" ID="ID_545673410" CREATED="1583314200454" MODIFIED="1583458744047">
+<hook EQUATION="\lambda = \dfrac{hc}{eU} \approx 0.1\ \mathrm{nm}" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+<node TEXT="(2) &#x7535;&#x5b50;&#x884d;&#x5c04;" ID="ID_1024824473" CREATED="1583314215540" MODIFIED="1583420284230">
+<hook EQUATION="\lambda = \dfrac{h}{p}\approx 0.1\ \mathrm{nm}" NAME="plugins/latex/LatexNodeHook.properties"/>
+<node TEXT="&#x6563;&#x5c04;&#x5f3a;, &#x900f;&#x5c04;&#x5f31;, &#x4e3b;&#x8981;&#x7528;&#x6765;&#x89c2;&#x5bdf;&#x8584;&#x819c;" ID="ID_986787125" CREATED="1583420609780" MODIFIED="1583458716147"/>
+</node>
+<node TEXT="(3) &#x4e2d;&#x5b50;&#x884d;&#x5c04;" ID="ID_1695376013" CREATED="1583314229683" MODIFIED="1583420577283">
+<hook EQUATION="\lambda = \dfrac{h}{p} \approx 0.1\ \mathrm{nm}" NAME="plugins/latex/LatexNodeHook.properties"/>
+<node TEXT="&#x5177;&#x6709;&#x78c1;&#x77e9;, &#x9002;&#x5408;&#x7814;&#x7a76;&#x78c1;&#x6027;&#x7269;&#x8d28;" ID="ID_855409354" CREATED="1583420583674" MODIFIED="1583420603464"/>
+</node>
+</node>
+<node TEXT="2). &#x884d;&#x5c04;&#x65b9;&#x7a0b;" ID="ID_842561961" CREATED="1583420660251" MODIFIED="1583460817446">
+<node TEXT="(1) &#x5e03;&#x62c9;&#x683c;&#x53cd;&#x5c04;&#x516c;&#x5f0f;" ID="ID_1878224752" CREATED="1583420675175" MODIFIED="1583571853082">
+<hook EQUATION="2 d_{h_{1}h_{2}h_{3}} \sin\theta = n\lambda&#xa;" NAME="plugins/latex/LatexNodeHook.properties"/>
+<font BOLD="false"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      (&#24050;&#30693;&#20837;&#23556;&#27874;&#21450;&#26230;&#38754;)
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="\latex \jlmText{&#x6709;&#x6548;&#x884d;&#x5c04;&#x6761;&#x4ef6;:} $\lambda \leq \dfrac{2d}{n}$" ID="ID_1186284004" CREATED="1583420855270" MODIFIED="1583457802903">
+<font BOLD="false"/>
+</node>
+</node>
+<node TEXT="(2) &#x52b3;&#x5384;&#x884d;&#x5c04;&#x65b9;&#x7a0b;" ID="ID_960572533" CREATED="1583420815115" MODIFIED="1583806576959">
+<font BOLD="false"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      (&#24050;&#30693;&#20837;&#23556;&#27874;&#21450;&#20986;&#23556;&#27874;)
+    </p>
+  </body>
+</html>
+</richcontent>
+<hook EQUATION="\begin{align}&#xa;    &amp;\jlmText{&#x539f;&#x59cb;&#x5f62;&#x5f0f;:} &amp;\vec{R}\cdot(\vec{S} - \vec{S}_0) = m\lambda \\&#xa;    &amp;\jlmText{&#x66f4;&#x5177;&#x542f;&#x53d1;&#x6027;&#x7684;&#x5f62;&#x5f0f;:} &amp;\vec{k}_0 - \vec{k} = \vec{G}&#xa;\end{align}&#xa;" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+<node TEXT="(3) &#x7531;&#x52b3;&#x5384;&#x65b9;&#x7a0b;&#x63a8;&#x5bfc;&#x5e03;&#x62c9;&#x683c;&#x53cd;&#x5c04;&#x516c;&#x5f0f;" ID="ID_290356455" CREATED="1583458059212" MODIFIED="1583461036567"/>
+<node TEXT="(4) &#x53cd;&#x5c04;&#x7403;" LOCALIZED_STYLE_REF="default" ID="ID_305745263" CREATED="1583461240445" MODIFIED="1583572219623">
+<hook EQUATION="\begin{align}&#xa;    &amp; \jlmText{&#x5df2;&#x77e5;&#x5165;&#x5c04;&#x6ce2;&#x65b9;&#x5411;}\ \vec{k}_0\jlmText{, &#x4ee5;&#x6ce2;&#x77e2;&#x957f;&#x4e3a;} \\&#xa;    &amp; \jlmText{&#x534a;&#x5f84;&#x753b;&#x7403;, &#x5219;&#x5012;&#x683c;&#x70b9;&#x53cd;&#x5c04;&#x7684;&#x5145;&#x8981;} \\&#xa;    &amp; \jlmText{&#x6761;&#x4ef6;&#x4e3a;&#x5012;&#x683c;&#x70b9;&#x5728;&#x7403;&#x9762;&#x4e0a;.}&#xa;\end{align}&#xa;" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+</node>
+<node TEXT="3). &#x6676;&#x4f53; X &#x5c04;&#x7ebf;&#x884d;&#x5c04;&#x7684;&#x51e0;&#x79cd;&#x65b9;&#x6cd5;" ID="ID_990959979" CREATED="1583551455659" MODIFIED="1583551474785">
+<node TEXT="(1) &#x52b3;&#x5384;&#x6cd5;" ID="ID_1313219024" CREATED="1583551767777" MODIFIED="1583553491216"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21333;&#26230;&#19981;&#21160;, &#20837;&#23556;&#20809;&#26041;&#21521;&#19981;&#21464;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="(2) &#x8f6c;&#x52a8;&#x5355;&#x6676;&#x6cd5;" ID="ID_1204420184" CREATED="1583551778107" MODIFIED="1583553501754" TEXT_ALIGN="LEFT"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21333;&#33394; X &#20809;, &#35753;&#21333;&#26230;&#36716;&#21160;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="(3) &#x7c89;&#x672b;&#x6cd5;" ID="ID_1726479314" CREATED="1583551785197" MODIFIED="1583553538987"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21333;&#33394; X &#20809;, &#21462;&#21521;&#21508;&#24322;&#30340;&#21333;&#26230;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
